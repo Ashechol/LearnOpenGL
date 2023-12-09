5ecb5733b2f9c00c27c1b7a881507d2898383810
Ashechol <ashechol@qq.com>
2023-12-09 16:38:05 +0800

    update

--- Source/HelloTriangle.cpp
+++ Source/HelloTriangle.cpp
@@ -16,10 +16,23 @@ void processInput(GLFWwindow* window)
 }
 
 // 三角形三个点的坐标
+// float vertices[] = {
+//         -0.5f, -0.5f, 0.0f,
+//          0.5f, -0.5f, 0.0f,
+//          0.0f,  0.5f, 0.0f
+// };
+
+// 矩形四个点的坐标
 float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+};
+
+unsigned int indices[] = {
+        0, 1, 3,    // 第一个三角形
+        1, 2, 3     // 第二个三角形
 };
 
 const char* vertexShaderSource = "#version 330 core\n"
@@ -43,7 +56,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
@@ -56,6 +68,7 @@ int main()
         return EXIT_FAILURE;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
     {
@@ -64,16 +77,19 @@ int main()
     }
 
     // 前两个值表示窗口左下角的位置
-    // glViewport(0, 0, 1600, 1200);
-    glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
+#ifdef __APPLE__
+    glViewport(0, 0, 1600, 1200);   // Retina 显示器下，宽高翻倍
+#else
+    glViewport(0, 0, 800, 600);
+#endif
+
+    int success;
+    char infoLog[512];
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
-
-    int success;
-    char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 
     if (!success)
@@ -86,6 +102,13 @@ int main()
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "Error::Shader::Fragment::Compilation_Failed\n" << infoLog << std::endl;
+    }
 
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
@@ -99,25 +122,34 @@ int main()
         glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
         std::cout << infoLog << std::endl;
     }
-
-    glUseProgram(shaderProgram);
+    // 着色器成功绑定连接到着色程序上后，就可以释放它们的内存了
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Vertex Array Objects, Vertex Buffer Objects
-    unsigned int VAO, VBO;
+    // Vertex Array Objects, Vertex Buffer Objects, Element(Index) Buffer Objects
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    // // 通常情况没有解绑的必要，因为每次配置新的 VAO 和 VBO 时都会绑定对应的。
+    // glBindBuffer(GL_ARRAY_BUFFER, 0);   // 解绑 VBO
+    // glBindVertexArray(0);               // 解绑 VAO
 
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // 渲染循环
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // 设置清空屏幕颜色缓冲时的颜色，状态设置函数
@@ -125,7 +157,7 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES,  6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
